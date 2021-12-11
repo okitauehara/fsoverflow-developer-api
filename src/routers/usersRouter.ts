8faf44812ddc02b5e221b0897778a222ee866d4e
okitauehara <marcosokita@live.com>
2021-12-10 22:35:43 -0300

    Fixing usersRouter import and function

--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import * as usersController from '../controllers/questionsController';
+import * as usersController from '../controllers/usersController';
 
 const router = Router();
 
-router.post('/', usersController.postQuestion);
+router.post('/', usersController.postUser);
 
 export default router;
